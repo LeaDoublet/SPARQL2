b2fa4d9caa0e6711955faee5bafa35890b27e826
Léa Doublet <leadbtcode@gmail.com>
2026-06-01 21:23:40 +0200

    age is greater than or equals to 24 years old

--- src/main/java/org/doublet/Querying_SPARQL2.java
+++ src/main/java/org/doublet/Querying_SPARQL2.java
@@ -33,6 +33,7 @@ class Querying_SPARQL {
 			// Create an empty in-memory model and populate it from the graph
 			model = ModelFactory.createMemModelMaker().createFreshModel();//.createModel();
 			model.read(in,null); // null base URI, since model URIs are absolute
+			model.read(in2,null);
 			in.close();
 		}catch (Exception e) {
 			System.err.println( "Error! : FILE NOT FOUND (" + e + ")" );
@@ -75,6 +76,7 @@ class Querying_SPARQL {
     		"}";
 			 */
 		//Query Change the query just a little to return the anonymous node as well?
+				/*
 				"PREFIX vcard: <http://www.w3.org/2001/vcard-rdf/3.0#> " +
 						"SELECT ?nameNode ?givenName " +
 						"WHERE { " +
@@ -82,6 +84,17 @@ class Querying_SPARQL {
 						"  ?nameNode vcard:Family \"Smith\" . " +
 						"  ?nameNode vcard:Given ?givenName . " +
 						"}";
+
+				 */
+		//Query Find the URI of all people in the knowledge base whose age is greater than or equals to 24 years old.
+				"PREFIX info: <http://somewhere/peopleInfo#> " +
+						"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
+						"SELECT ?person " +
+						"WHERE { " +
+						"  ?person info:age ?age . " +
+						"  FILTER(?age >= 24) " +
+						"}";
+
 		Query query = QueryFactory.create(queryString);
 
 		// Execute the query and obtain results

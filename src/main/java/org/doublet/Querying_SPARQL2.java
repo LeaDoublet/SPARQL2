b3b7874b808f4075d484f12a30126e504bebbc20
Léa Doublet <leadbtcode@gmail.com>
2026-06-01 21:07:58 +0200

    Initial commit

---
+++ src/main/java/org/doublet/Querying_SPARQL2.java
@@ -0,0 +1,78 @@
+package org.doublet;
+
+import java.io.*;
+
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+//import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+
+
+
+class Querying_SPARQL {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Model model;
+		try{
+			// Open the bloggers RDF graph from the filesystem
+			InputStream in = new FileInputStream( new File( "C:/Users/ldoublet/Documents/FIA5/periode 2/vc-db-1.rdf" ));
+			InputStream in2 = new FileInputStream(new File("C:/Users/ldoublet/Documents/FIA5/periode 2/vc-db-2.rdf"));
+ 
+
+
+			// Create an empty in-memory model and populate it from the graph
+			model = ModelFactory.createMemModelMaker().createFreshModel();//.createModel();
+			model.read(in,null); // null base URI, since model URIs are absolute
+			in.close();
+		}catch (Exception e) {
+			System.err.println( "Error! : FILE NOT FOUND (" + e + ")" );
+			return;
+		}
+		// Create a new query
+		String queryString = 
+				
+		//Query With full URI
+				/*
+		        "SELECT ?x "+
+				"WHERE { " +
+		                  "?x  <http://www.w3.org/2001/vcard-rdf/3.0#FN>  \"John Smith\" .}";
+				*/
+		//Query With Prefix
+				/*
+			    "PREFIX vcard: <http://www.w3.org/2001/vcard-rdf/3.0#> " +
+		        "SELECT ?x "+
+				"WHERE { " +
+		                  "?x  vcard:FN  \"John Smith\" .}";
+
+				 */
+		//Query With full name of all people in the database
+				"PREFIX vcard: <http://www.w3.org/2001/vcard-rdf/3.0#> " +
+				"SELECT ?x ?name " +
+				"WHERE { " +
+				"  ?x vcard:FN ?name . " +
+				"} " +
+				"ORDER BY ?name";
+		Query query = QueryFactory.create(queryString);
+
+		// Execute the query and obtain results
+		QueryExecution qe = QueryExecutionFactory.create(query, model);
+		ResultSet results = qe.execSelect();
+
+		// Output query results	
+		ResultSetFormatter.out(System.out, results, query);
+
+		// Important - free up resources used running the query
+		qe.close();
+	}
+
+}
